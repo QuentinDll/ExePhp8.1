d7392a7a28186386dc4c27e4eb1f210901acde39
QuentinDll <quentin.delplace13@gmail.com>
2018-07-26 16:23:25 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 8</title>
+</head>
+<body>
+<p>
+<?php
+$res1 = 3 + 4;
+echo $res1;
+$res2 = 5 * 20;
+echo $res2;
+$res3 = 45 / 5;
+echo $res3;
+ ?>
+</p>
+</body>
+</html>
